146ddc65e5725249a046a40287c8ab566c3afd80
Warren Niu <warrenniu@gmail.com>
2020-09-28 18:04:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,9 +17,10 @@ empty_hash = {}
 empty_hash
 end
 
-# def get_japanese_emoticon
-#   # code goes here
-# end
+def get_japanese_emoticon
+  # code goes here
+  
+end
 #
 # def get_english_meaning
 #   # code goes here
