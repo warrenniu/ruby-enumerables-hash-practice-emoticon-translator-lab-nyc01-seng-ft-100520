e61b48b71812bdfcfb4ca8eebf599a75e53e1a7d
Warren Niu <warrenniu@gmail.com>
2020-09-28 17:25:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,7 @@ empty_hash = {}
 
   emoticons.each do |key, value|
     empty_hash[key] = {}
-      :english = empty_hash[key][0]
-      :japanese = empty_hash[key][1]
+      
     binding.pry
     end
 empty_hash
