8b708018fdb3236babc3cc116787021f0a004293
Warren Niu <warrenniu@gmail.com>
2020-09-28 18:41:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,8 @@ end
 def get_japanese_emoticon(emoticons, value)
   # code goes here
   load_library(emoticons)
+
+  empty_hash = {}
     # emoticons.each do |key, value|
       # if emoticons[key] == [value]
       # return value
