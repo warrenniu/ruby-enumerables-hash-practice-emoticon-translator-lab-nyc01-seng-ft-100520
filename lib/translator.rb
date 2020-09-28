b3df5da50d9bbb697b58ce2c36e15377208424b5
Warren Niu <warrenniu@gmail.com>
2020-09-28 18:26:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ end
 def get_japanese_emoticon(emoticons, value)
   # code goes here
   load_library(emoticons)
-    if emoticons.each do |value|
+    if emoticons.each do |key, value|
       binding.pry
 
 end
