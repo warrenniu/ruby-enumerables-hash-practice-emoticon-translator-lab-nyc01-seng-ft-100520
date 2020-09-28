acc09c4662257c200c146d40cdd21a9c6849ddb6
Warren Niu <warrenniu@gmail.com>
2020-09-28 19:44:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon(emoticons, value)
       binding.pry
         value.each do |inner_key[1], inner_value[1]|
 
-            return inner_value
+            return inner_value[1]
           # binding.pry
 
       end
