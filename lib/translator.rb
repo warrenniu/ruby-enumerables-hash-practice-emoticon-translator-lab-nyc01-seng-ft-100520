422540dbb4482b63fc111686e54648f78e808d52
Warren Niu <warrenniu@gmail.com>
2020-09-28 19:42:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,8 +22,9 @@ def get_japanese_emoticon(emoticons, value)
   load_library(emoticons)
 
     load_library(emoticons).each do |key, value|
+      binding.pry
         value.each do |inner_key, inner_value|
-          
+
             return inner_value
           # binding.pry
 
