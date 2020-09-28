f49101d23ac8e10f811bbd32b44535bf0887c81d
Warren Niu <warrenniu@gmail.com>
2020-09-28 15:32:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,20 @@
 # require modules here
+require "pry"
+require "yaml"
 
-def load_library
+
+def load_library(cats)
   # code goes here
+emoticons = YAML.load_file(cats)
+# binding.pry
+
+emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
+# def get_japanese_emoticon
+#   # code goes here
+# end
+#
+# def get_english_meaning
+#   # code goes here
+# end
